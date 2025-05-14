f360b3b00f03ce16c4ab4834d47ba91bb206cc96
sabbir1257 <masabbir1257@gmail.com>
2025-05-14 12:17:00 +0600

    Concept of createAsyncThunk

--- src/redux/features/user/userSlice.js
+++ src/redux/features/user/userSlice.js
@@ -1,12 +1,20 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const initialState = {
-  name: 'Mir Hussain',
-  email: 'mir@gmail.com',
+  name: "",
+  email: "",
+  isLoading: true,
+  isError: false,
+  error: "",
 };
 
+export const createUser = createAsyncThunk("userSlice/createUser", async () => {
+  createUserWithEmailAndPassword();
+});
+
 const userSlice = createSlice({
-  name: 'userSlice',
+  name: "userSlice",
   initialState,
   reducers: {},
 });
